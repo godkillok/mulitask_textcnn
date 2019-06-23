79892802dc1dd2ce787709e14543c36029cd48b0
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-06-24 00:03:04 +0800

    t

--- base_model/cnn_model.py
+++ base_model/cnn_model.py
@@ -21,7 +21,7 @@ def gelu(input_tensor):
 
 
 class CnnModel(Model):
-    def __init__(self, config, input_ids, author_id, category_ids, keyword_ids, keyword_label_ids, is_training):
+    def __init__(self, config, input_ids, author_id, category_ids,  is_training):
         super(CnnModel, self).__init__(config, input_ids, is_training)
         self.author_id = author_id
         self.category_ids = category_ids
