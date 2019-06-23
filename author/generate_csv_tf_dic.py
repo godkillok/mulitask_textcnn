542469f32212cfec3a4935668a3fbd2e1d55a148
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-06-23 16:51:01 +0800

    t

--- author/generate_csv_tf_dic.py
+++ author/generate_csv_tf_dic.py
@@ -6,18 +6,21 @@ import numpy as np
 import random
 
 import random
+import json
 from common_tool import per_line
 flags = tf.app.flags
 flags.DEFINE_string("data_dir", "/data/tanggp/youtube8m/", "Directory containing the dataset")
 flags.DEFINE_string("pad_word", "0", "used for pad sentence")
 flags.DEFINE_string("path_vocab", "/data/tanggp/youtube8m/textcnn_words.txt", "used for word index")
 flags.DEFINE_string("path_author",  os.path.join("/data/tanggp/youtube8m/", 'textcnn_author_sort'))
+flags.DEFINE_string("path_label",  os.path.join("/data/tanggp/youtube8m/", 'textcnn_label_sort'))
 FLAGS = flags.FLAGS
 
 sentence_max_len = 200
 pad_word = FLAGS.pad_word
 
 label_class=[]
+author_calss=[]
 def feature_auto(value):
     if isinstance(value, int):
         return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
@@ -44,24 +47,15 @@ def feature_auto(value):
         return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
 
 
-def parse_line_dict(record):
-    fields = record.strip().split(",")
+def parse_line_dict(record,vocab_dict,author_dict,label_dict):
+    tokens=per_line(record)
+    tokens=tokens.split()
+    text = [vocab_dict.get(r) for r in tokens if vocab_dict.get(r, '-99') != '-99']
+    record=json.loads(record)
+    label=record.get("label")
+    author=record.get("author")
 
-    if len(fields) < 2:
-        raise ValueError("invalid record %s" % record)
-    text = fields[1].strip().lower()
-    tokens = text.split()
-      # type: int
-    if len(fields) == 2:
-        label=0
-    else:
-        try:
-            label = int(fields[-1]) - 1
-        except:
-            print(fields)
-            print(len(fields))
-    label_class.append(label)
-    return [tokens, label]
+    return [text, label_dict.get(label),author_dict.get(author)]
 
 
 def per_thouds_lines_dict(result_lines, vocab, path_text, count,flag_name=''):
@@ -69,28 +63,25 @@ def per_thouds_lines_dict(result_lines, vocab, path_text, count,flag_name=''):
 
     rl_num=0
     for rl in result_lines:
-
-        rl_num+=1
-        jo=0
-
-        text = [vocab.get(r) for r in rl[0] if vocab.get(r, '-99')!='-99' ]
+        text=rl[0]
         label=rl[1]
+        author=rl[2]
         if len(text) >= sentence_max_len:
             text = text[0: sentence_max_len]
         else:
-            text += [0] * (sentence_max_len - len(text))
-        g={"text":text,"label":label}
+            text += [pad_word] * (sentence_max_len - len(text))
+        g={"text":text,"label":label,"author":author}
         tf_lines.append(g)
 
 
 
         if rl_num%1000==0:
             flag_name=str(rl_num)
-            write_tfrecords(tf_lines, path_text, count, flag_name)
+            write_tfrecords(tf_lines, path_text, count)
             tf_lines = []
     if len(tf_lines)>0:
         flag_name = str(rl_num)
-        write_tfrecords(tf_lines, path_text, count,flag_name)
+        write_tfrecords(tf_lines, path_text, count)
             # tf_lines=[]
 
 def generate_tf_dic(path_text):
@@ -98,28 +89,31 @@ def generate_tf_dic(path_text):
         lines = f.readlines()
         vocab = {l.strip(): i for i, l in enumerate(lines)}
 
+    with open(FLAGS.path_author, 'r', encoding='utf8') as f:
+        lines = f.readlines()
+        author_dict = {l.strip(): i for i, l in enumerate(lines)}
+
+    with open(FLAGS.path_label, 'r', encoding='utf8') as f:
+        lines = f.readlines()
+        label_dict = {l.strip(): i for i, l in enumerate(lines)}
+
     result_lines = []
     count = 0
     with open(path_text, 'r', encoding='utf8') as f:
         lines = f.readlines()
         for line in lines:
-            field=line.split(',')
-            count += 1
-            try:
-                int(field[0])
-            except:
-                continue
-            result_lines.append(parse_line_dict(line))
+            result_lines.append(parse_line_dict(line,author_dict,label_dict))
             if count % 20000 == 0 or count == len(lines) - 1:
                 print(count)
                 per_thouds_lines_dict(result_lines, vocab, path_text, count)
                 result_lines = []
 
 
-def write_tfrecords(tf_lines, path_text, count,flag_name):
+def write_tfrecords(tf_lines, path_text, count):
     (root_path, output_filename) = os.path.split(path_text)
     output_filename = output_filename.split('.')[0]
-    output_file = output_filename + '_' + str(count) + '_' + str(0) +'_' + flag_name+ '.tfrecords'
+    output_filename+='text_cnn_'
+    output_file = output_filename + '_' + str(count)+ '.tfrecords'
 
     print("Start to convert {} to {}".format(len(tf_lines), os.path.join(root_path, output_file)))
 
