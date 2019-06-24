917db7afe4b92143d115ad3e530ee2489c496634
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-06-24 21:00:01 +0800

    t

--- author_v2/main_label_classify.py
+++ author_v2/main_label_classify.py
@@ -164,9 +164,12 @@ if __name__ == '__main__':
         logger.info("after train and evaluate")
     if FLAGS.do_predict == True:
         best_dir = model_dir + '/best'
-        with open(FLAGS.data_dir + '/label2id.pkl', 'rb') as rf:
-            label2id = pickle.load(rf)
-            id2label = {value: key for key, value in label2id.items()}
+
+        path_label=os.path.join(FLAGS.data_dir, 'textcnn_label_sort')
+        with open(FLAGS.path_label, 'r', encoding='utf8') as f:
+            lines = f.readlines()
+            id2label = { i:l.strip().split("\x01\t")[0] for i, l in enumerate(lines)}
+
         # predict
         predict_label_list = []
         true_label_list = []
