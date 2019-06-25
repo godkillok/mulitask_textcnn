fc3368ee2d4d853a785c8df9ccc988f5c5b8c764
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-06-25 22:58:44 +0800

    t

--- author/common_tool.py
+++ author/common_tool.py
@@ -3,16 +3,21 @@
 import re
 import json
 punctuation = r"""!"#$%&()*+,-./:;<=>?@[\]^_`{|}~。，"""
-
 def clean_str(text):
     text = text.strip()
     text = re.sub(r'[{}]+'.format(punctuation), ' ', str(text))
     text=' '.join(text.split())
+
     return text
 
 def per_line(line):
     li = json.loads(line)
-    pic_tags = [pic.split('\x02')[0] for pic in li.get('pic_tags', '').split('\x03')]
+    if  isinstance(li.get('pic_tags', ''),str):
+        pic_tags = [pic.split('\x02')[0] for pic in li.get('pic_tags', '').split('\x03')]
+    elif isinstance(li.get('pic_tags', ''),list):
+        pic_tags =li.get('pic_tags', '')
+    else:
+        pic_tags =[""]
     fields = li.get("title", "") + ' ' + ' '.join(li.get("tags", [""])) + ' '.join(pic_tags)
     text = clean_str(fields.lower())
     text = text.replace("\n", ' ').replace("\r", " ")
